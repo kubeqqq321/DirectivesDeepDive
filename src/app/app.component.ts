6b06abe90267de08bdfead47b42cb357a104cbd4
Jakub.Marciniak <kubeqqq321@hotmail.com>
2024-10-15 16:36:22 +0200

    (149) -> Angular - The Complete Guide (2024 Edition)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LearningResourcesComponent} from './learning-resources/learning-resources.component';
+import {AuthComponent} from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LearningResourcesComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
